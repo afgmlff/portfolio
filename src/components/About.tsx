ccdc3144fbeefdcc387d1637932ed5c2ea6ae787
afgmlff <aeciofgm@gmail.com>
2022-11-27 15:35:01 -0300

    remove IonContent when not needed (about tsx)

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -31,14 +31,22 @@ useEffect(() => {
 
     return (
     <>
-        <IonContent className="ion-no-padding ion-no-margin copyableText" forceOverscroll={false}>
-            <IonGrid className="ion-no-padding ion-no-margin">
+
+            <IonGrid className="ion-no-padding ion-no-margin copyableText">
                 <IonRow class="ion-no-margin"><div className='transitionDiv'></div></IonRow>
                 <IonRow>
-                <IonCol><p className='contentTitle'>About</p></IonCol>
+                <IonCol><p className='contentTitle'><span className="meColor">A</span>bout</p></IonCol>
+                </IonRow>
+                <IonRow class="ion-padding">
+                    <IonCol>
+                        <p className='aboutText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </IonCol>
+                    <IonCol>
+                        <img className='aboutIcon' src="./assets/icon/me.png"></img>
+                    </IonCol>
                 </IonRow>
             </IonGrid>
-        </IonContent>
+ 
     </>
     )
 }
