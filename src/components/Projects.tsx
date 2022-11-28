4b7a54a62a8f83f34c1ee063867ea3fc2e2b5e47
afgmlff <aeciofgm@gmail.com>
2022-11-28 20:03:28 -0300

    first project icons

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -41,11 +41,11 @@ useEffect(() => {
                 </IonRow>
         
                 <IonRow className="ion-padding fadeIn1 dskmob margTop"> {/* change class name later. */}
-                <IonCol className='ion-text-left'>
+                <IonCol className='ion-text-left' size='6'>
                         
                         <img className='projLeft' src="./assets/wordleGif.gif"></img>
                 </IonCol>
-                <IonCol>
+                <IonCol size='3' offset='-2'>
                     <IonCard>
                         <IonCardHeader>
                             <IonCardTitle><span className='boldT'>wanna be Wordle</span></IonCardTitle>
@@ -53,7 +53,15 @@ useEffect(() => {
                         </IonCardHeader>
 
                         <IonCardContent>
-                            Here's a small text description for the card content. Nothing more, nothing less.
+                            A project that shares the same logic as "Wordle" (The New York Times). I wanted to learn Angular in practice, but had no creativity at the time.
+
+                        </IonCardContent>
+
+                        <IonCardContent>
+                            <img className="cardIcon" src="./assets/Angular.png"/>
+                            <img className="cardIcon" src="./assets/Typescript.png"/>
+                            <img className="cardIcon" src="./assets/html5-logo.png"/>
+                            <img className="cardIcon" src="./assets/css.png"/>
                         </IonCardContent>
                         <IonButton fill="clear">Action 1</IonButton>
                         <IonButton fill="clear">Action 2</IonButton>
